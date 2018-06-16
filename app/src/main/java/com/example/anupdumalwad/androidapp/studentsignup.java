6c3f2417268bc71c2bb24bc0989f11efd40bfe56
anuprao1 <anuprao@iitk.ac.in>
2018-06-16 17:14:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/anupdumalwad/androidapp/studentsignup.java
@@ -0,0 +1,13 @@
+package com.example.anupdumalwad.androidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class studentsignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studentsignup);
+    }
+}
